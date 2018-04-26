77883b5726f8aff6256f6457f33b9ff7e0fd4cf2
Chak_Tang <chakl@hotmail.com>
2018-04-26 17:44:56 +0200

    Added mineBlock function in Block.java
    Mine a block based on difficulty and nonce

--- src/Block.java
+++ src/Block.java
@@ -7,23 +7,45 @@ public class Block {
 	public String previousHash;
 	public long timeStamp;
 	private String data;
+	private int nonce;
 	
 	
 	//Block constructor
-	public Block(int blocknumber, String previousHash, String data) {
+	public Block(int blocknumber, String previousHash, String data,int difficulty) {
 		this.blocknumber = blocknumber;
 		this.previousHash = previousHash;
 		this.timeStamp = Instant.now().toEpochMilli();
 		this.data = data;
-		this.hash = calculateHash();
+		this.hash = "?????????????????????????????";
+		this.hash = mineBlock(this.hash,difficulty);
 	}
 	
-	
+
 	//Helper function for calculating the hash
-	public String calculateHash() {
+	public String calculateHash(int nonce) {
 		
-		String stringtoHash = previousHash + Long.toString(timeStamp) + data;
+		String stringtoHash = previousHash + Long.toString(timeStamp) + data + nonce;
 		String hashedString = HashSHA256.sha256(stringtoHash);
 		return hashedString;
+	}
+	
+
+
+	public String mineBlock(String hash,int difficulty) {
+		
+		//Create a string with leading zeros
+		String target = new String(new char[difficulty]).replace('\0', '0');
+		
+		System.out.println("Block number : " + this.blocknumber);
+		
+		//Each time no match for hash, add increase nonce and add to the string to hash.
+		while(!hash.substring(0, difficulty).equals(target)) {
+			nonce++;
+			hash = calculateHash(nonce);
+			System.out.println("hash : " + hash);
+		}
+
+		System.out.println("Block Mined!!! : " + hash+"\n");
+		return hash;
 	}
 }
--- src/Blockchain.java
+++ src/Blockchain.java
@@ -7,16 +7,16 @@ public class Blockchain {
 	
 	public static void main(String[] args) {
 		
-		blockchain.add(new Block(blockchain.size()+1,"0","Testing with genesis block"));
-		blockchain.add(new Block(blockchain.size()+1,blockchain.get(blockchain.size()-1).hash,"Testing with second block"));
-		blockchain.add(new Block(blockchain.size()+1,blockchain.get(blockchain.size()-1).hash,"Testing with third block"));
+		blockchain.add(new Block(blockchain.size()+1,"0","Testing with genesis block",1));
+		blockchain.add(new Block(blockchain.size()+1,blockchain.get(blockchain.size()-1).hash,"Testing with second block",2));
+		blockchain.add(new Block(blockchain.size()+1,blockchain.get(blockchain.size()-1).hash,"Testing with third block",3));
 		
 		
 		//Json views
 		String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);		
 		System.out.println(blockchainJson);
 
-		System.out.println(validChain());
+		System.out.println("The chains are valid: "+validChain());
 		
 	}
 	
@@ -32,6 +32,8 @@ public class Blockchain {
 				return false;
 			}
 		}
+
 		return true;
 	}
+
 }
